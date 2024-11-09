be2a2fe9a2b599699ca3663a61672e1aeccde4a5
eraddare <delacerna.aclc@gmail.com>
2024-11-09 23:19:38 +0800

    updated

---
+++ app/Models/Cases.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cases extends Model
+{
+    protected $table = 'case';
+    protected $fillable = ['description']; 
+}
