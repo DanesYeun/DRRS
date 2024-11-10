65cfff715ae90728d63d8cd14ae81e3387b990e2
danes <dadadanesss@gmail.com>
2024-11-10 20:33:53 +0800

    front-end cleanup and modifications

--- resources/views/pages/users/addUser.blade.php
+++ resources/views/pages/users/addUser.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
     <div class="d-flex flex-column m-md-2">
-        <h3 class="text-start mx-2 text-primary">Add User</h3>      
+        <div class="d-flex flex-row justify-content-between">
+            <h3 class="text-start mx-2 text-primary">Add User</h3>
+            <a class="btn btn-danger col-4 col-md-2 mb-3 " href="{{ route('users') }}">
+                <i class="bi bi-backspace-fill p-2"></i>
+                Back
+            </a>
+        </div>     
         <div class="mx-2 mb-3 p-2">
             <form method="post" action="{{ route('add-user') }}" class="needs-validation" novalidate>
                 @csrf
                 <div class="border container bg-white rounded row mx-2 p-3">
+                    <h4 class="py-2 text-primary text-start">User Information</h4>
+
                     <x-input name="firstname" label="First Name" type="text"/>
                     <x-input name="lastname" label="Last Name" type="text"/>
 
