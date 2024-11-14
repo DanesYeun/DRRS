37ea94b7a61d8d8c4fffc73dff18e61426e4f337
danes <dadadanesss@gmail.com>
2024-11-14 13:52:45 +0800

    fixed error on adding response record on null $data

--- resources/views/pages/responseRecords/add.blade.php
+++ resources/views/pages/responseRecords/add.blade.php
@@ -9,9 +9,9 @@
                 
                 <div class="border container bg-white rounded row mx-2 px-3 pt-5 pb-2">
                     <h4 class="py-2 text-primary text-start">Response Details</h4>
-                   
-                    <x-input name="date" label="Date" type="date" value="{{ date('Y-m-d', strtotime($data->created_at)) ?? '' }}"/>
-                    <x-input name="time" label="Time" type="time" value="{{ date('H:i', strtotime($data->created_at)) ?? ''}}"/>
+      
+                    <x-input name="date" label="Date" type="date" value="{{ date('Y-m-d', strtotime(isset($data->created_at))) ?? '' }}"/>
+                    <x-input name="time" label="Time" type="time" value="{{ date('H:i', strtotime(isset($data->created_at))) ?? ''}}"/>
 
                     <x-select name="incidentFrom" label="Incident From" :options="$locations" required="true"/>
                     <x-select name="takenTo" label="Taken To" :options="$locations" required="true"/>
@@ -24,7 +24,7 @@
                     <x-input name="patientAddress" label="Address" type="text" value="{{ $data->patientAddress ?? ''}}"/>
                     <x-select name="patientCase" label="Case" :options="$cases" required="true" value="{{ $data->case ?? ''}}"/> 
 
-                    <x-input name="responders" label="Responders" type="text" value="{{ $data->recorded_by->firstname.' '.$data->recorded_by->lastname }}"/>
+                    <x-input name="responders" label="Responders" type="text"  value="{{ isset($data->recorded_by) ? $data->recorded_by->firstname . ' ' . $data->recorded_by->lastname : '' }}"/>
                     <x-input name="actionTaken" label="Action Taken" type="text"/>
 
                     <x-textarea label="Remarks" name="remarks"/>
